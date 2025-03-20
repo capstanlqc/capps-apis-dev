046d22ceeb9748ac8ec985f4d51483d3bfe00e3c
gergoe <gergoe@harras.be>
2025-03-20 16:25:16 +0100

    Added analytic_account and created_at when creating new entries, changed the order of calls request so it returns the last 10 entries (not the first ten), added get_aggregated_data which is called by the frontend to get the accumulated data based on the input parameters

--- code/models/mt_usage.py
+++ code/models/mt_usage.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel
 
 
@@ -8,7 +10,24 @@ class CallIn(BaseModel):
     source_lang: str
     target_lang: str
     mt_provider: str
+    analytic_account: Optional[str] = None
+    created_at: Optional[datetime] = datetime.today()
 
 
 class Call(CallIn):
     oid: str
+
+
+class AccumulatedUsageRequest(BaseModel):
+    start_date: str  # Format: YYYY-MM-DD
+    end_date: str  # Format: YYYY-MM-DD
+    application: Optional[str] = None
+    analytic_account: Optional[str] = None
+    mt_provider: Optional[str] = None
+
+
+class AccumulatedUsageResponse(BaseModel):
+    total_chars: int
+    application: Optional[str] = None
+    analytic_account: Optional[str] = None
+    mt_provider: Optional[str] = None
--- code/routers/mt_usage.py
+++ code/routers/mt_usage.py
@@ -1,18 +1,19 @@
 import os
+from datetime import datetime
+
 from dotenv import load_dotenv
+from fastapi import APIRouter, HTTPException
+from code.models.mt_usage import (
+    Call,
+    CallIn,
+    AccumulatedUsageRequest,
+    AccumulatedUsageResponse
+)
+from motor.motor_asyncio import AsyncIOMotorClient
+from code.serializer import convert_doc, convert_doc_list
 
 load_dotenv()
 mongo_uri = os.getenv("MONGO_URI")
-
-from fastapi import APIRouter, HTTPException
-
-from code.models.mt_usage import (
-    Call,
-    CallIn
-)
-
-from motor.motor_asyncio import AsyncIOMotorClient
-from code.serializer import convert_doc, convert_doc_list
 
 client = AsyncIOMotorClient(mongo_uri)
 database = client.get_database("cappisdb")
@@ -34,6 +35,7 @@ async def root():
         "collections": collections,
     }
 
+
 @router.post("/calls")
 async def create_call_record(call: CallIn):
     await collection.insert_one(call.model_dump())
@@ -42,9 +44,58 @@ async def create_call_record(call: CallIn):
 
 @router.get("/calls", response_model=list[Call])
 async def read_call_records():
-    calls = await collection.find().to_list(length=10)
+    calls = await collection.find().sort({'created_at': -1}).to_list(length=10)
     return convert_doc_list(calls)
 
+
+@router.post("/stats", response_model=list[AccumulatedUsageResponse])
+async def get_aggregated_data(request: AccumulatedUsageRequest, group_by: str = "mt_provider"):
+    """
+    Reads accumulated statistics based on the given parameters.
+    Test with:
+    {
+      "start_date": "2025-01-01",
+      "end_date": "2026-01-01",
+      "application": null,
+      "mt_provider": null
+    }
+    :param request: AccumulatedUsageRequest
+    :param group_by: Field to use for grouping. Defaults to mt_provider
+    :return: List of statistics lines
+    """
+
+    matches = dict()
+    today = datetime.today()
+    try:
+        start_date = datetime.strptime(request.start_date, "%Y-%m-%d") if request.start_date else today.replace(day=1)
+        end_date = datetime.strptime(request.end_date, "%Y-%m-%d") if request.end_date else today
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid date format. Use YYYY-MM-DD.")
+
+    if start_date > end_date:
+        raise HTTPException(status_code=400, detail="Start date cannot be after end date")
+
+    matches["created_at"] = {"$gte": start_date, "$lte": end_date}
+
+    if request.application and request.application != "all":
+        matches["application"] = request.application
+
+    if request.analytic_account and request.analytic_account != "all":
+        matches["analytic_account"] = request.analytic_account
+
+    if request.mt_provider and request.mt_provider != "all":
+        matches["mt_provider"] = request.mt_provider
+
+    grouping = {
+        "_id": "$" + group_by, "total_chars": {"$sum": "$char_count"}
+    }
+    cursor = collection.aggregate([
+        {"$match": matches},
+        {"$group": grouping},
+        {"$project": {group_by: "$_id", "total_chars": 1, "_id": 0}}
+    ])
+    results = list(await cursor.to_list())
+    return results
 
 # @router.post("/calls", response_model=Call, status_code=201)
 # async def create_call_record(call: CallIn):
